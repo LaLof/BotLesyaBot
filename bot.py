ebc42622524b0b4b853108bd958d7086c8d82192
LaLof <50831327+LaLof@users.noreply.github.com>
2019-07-23 21:22:50 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,7 @@
+import pyautogui
+def bot(interval1):
+	pyautogui.typewrite('&#1082;&#1072;&#1079;&#1080;&#1085;&#1086; 135&#1082;&#1082;&#1082;', interval=interval1)
+
+	pyautogui.press('enter')
+while True:
+	bot(0.1)
