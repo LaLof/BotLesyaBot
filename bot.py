07774d1cf8f6296f4b493aca128d4e9f65d6c987
LaLof <50831327+LaLof@users.noreply.github.com>
2019-07-24 10:59:36 +0300

    Added auto lang change

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import pyautogui
+
 def bot(interval1):
 	pyautogui.typewrite('&#1082;&#1072;&#1079;&#1080;&#1085;&#1086; 135&#1082;&#1082;&#1082;', interval=interval1)
 
 	pyautogui.press('enter')
+
 while True:
 	bot(0.1)
+
+pyautogui.hotkey('shift','alt')
+ 
