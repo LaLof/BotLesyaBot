093db716f911f8b954e8f274b1e21b9f12fd859f
LaLof <guesscome@gmail.com>
2020-02-25 22:49:05 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,65 @@
-import pyautogui
+#Imports
+from pyautogui import typewrite, FAILSAFE, press
+from random import randint
+from time import sleep
+#Declare for what we need
+randbet = randint(200, 999)
+FAILSAFE = False
+##########################
 
-def bot(interval1):
-	pyautogui.typewrite('&#1082;&#1072;&#1079;&#1080;&#1085;&#1086; 135&#1082;&#1082;&#1082;', interval=interval1)
 
-	pyautogui.press('enter')
+class bots:
+    def bot(delay):
+        global randbet
+        typewrite("&#1082;&#1072;&#1079;&#1080;&#1085;&#1086; " + str(randbet)+ "&#1082;&#1082;&#1082;", interval=delay)
+        randbet = randint(135,700)
+        press('enter')
+    def GivenBetBot(delay):
+        typewrite("&#1082;&#1072;&#1079;&#1080;&#1085;&#1086; " + str(playerbet)+ "&#1082;&#1082;&#1082;", interval=delay)
+        press("enter")
+    def getdev():
+        print("wait...")
+        sleep(10)
+        print("wait...")
+        sleep(5)
+        print("vk.com/lalof2")
+        input()
 
-while True:
-	bot(0.1)
 
-pyautogui.hotkey('shift','alt')
- 
+delay = input("Delay for every symbol written ")
+if delay == "dev":
+    getdev()
+
+
+boton = input("Turn on bot? yes/no/count/givenbet/givencount ")
+
+if boton == "yes":
+    sleep(5)
+    while True:
+        bots.bot(delay)
+
+elif boton == "dev":
+    bots.getdev()
+
+elif boton == "count":
+    betscount = input("How many bets you want to place? ")
+    sleep(5)
+    for i in range(int(betscount)):
+        bots.bot(delay)
+
+elif boton == "givenbet":
+    print("In this mode your bet will spam and not change or randomise")
+    playerbet = input("Your bet: ")
+    sleep(5)
+    while True:
+        bots.GivenBetBot(delay)
+
+elif boton == "givencount":
+    givenbetscount = input("How many bets you want to place? ")
+    playerbet = input("Your bet: ")
+    sleep(5)
+    for i in range(int(givenbetscount)):
+        bots.GivenBetBot(delay)
+
+else:
+    print("script completed")
